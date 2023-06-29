1b600c568e5272e3aee520e01aaa398a36326998
RZorkin <zorkin81@gmail.com>
2023-06-29 16:48:15 +0300

    Add project files

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -11,7 +11,11 @@ export const Navigation = () => {
       <NavLink className={css.link} to="/">
         Home
       </NavLink>
-      {isLoggedIn && <NavLink to="/contacts">Phonebook</NavLink>}
+      {isLoggedIn && (
+        <NavLink className={css.link} to="/contacts">
+          Phonebook
+        </NavLink>
+      )}
     </nav>
   );
 };
